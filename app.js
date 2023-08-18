6bd7096684329a51fa33e4fb7b40880353eb4a86
FRFRANk-1 <li.runch@northeastern.edu>
2023-08-17 21:31:41 -0400

    install cors, axios, express

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+
+
 import express from "express"; //import express library
+import cors from "cors"; //import cors library
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./users/users-controller.js";
 
 const app = express(); //create an express library
+app.use(cors()); //use cors library
 HelloController(app); //call the HelloController function and pass in the app object
 UserController(app); //call the UserController function and pass in the app object
 app.listen(4001);
