2e63d5521635186d67552eacc027efa15e55d057
FRFRANk-1 <li.runch@northeastern.edu>
2023-08-17 21:13:18 -0400

    testing server_1

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express"; //import express library
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./users/users-controller.js";
+
 const app = express(); //create an express library
-app.get("/", (req, res) => {
-  res.send("Hello World! o");
-});
+HelloController(app); //call the HelloController function and pass in the app object
+UserController(app); //call the UserController function and pass in the app object
 app.listen(4001);
