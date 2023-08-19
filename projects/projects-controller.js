55c3531bf0038e4c38e42e7b22424e977c2eec68
FRFRANk-1 <li.runch@northeastern.edu>
2023-08-19 11:01:01 -0400

    updated project and users controller(more postman)

--- projects/projects-controller.js
+++ projects/projects-controller.js
@@ -1,103 +1,74 @@
 import * as ProjectsDAO from "./projects-dao.js";
 
 const ProjectsController = (app) => {
-    app.post('/api/projects', createProject);
-    app.get('/api/projects', projectsFeed);
-    app.get('/api/projects/projectId', findProject);
-    app.get('/api/projects/courseId', findCourseProject);
-    app.get('/api/projects/courseId', coursesFeed);
-    app.get('/api/projects/projectId', myProjects);
-    app.put('/api/projects/projectId', updateProject);
-    app.delete('/api/projects/projectId', deleteProject);
+  app.post("/api/projects", createProject);
+  app.get("/api/projects/projectId", findProject);
+  app.get("/api/projects/courseId", findCourseProject);
+
+  app.get("/api/projects/projectId", myProjects);
+  app.put("/api/projects/projectId", updateProject);
+  app.delete("/api/projects/projectId", deleteProject);
 };
 
- const createProject = async (req, res) => {
-    try {
-        const project = await create(req.body);
-        res.status(201).json(project);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+const createProject = async (req, res) => {
+  try {
+    const project = await ProjectsDAO.create(req.body);
+    res.status(201).json(project);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
- const projectsFeed = async (req, res) => {
-    try {
-        const projects = await findAll();
-        res.json(projects);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+const findProject = async (req, res) => {
+  try {
+    const projectId = req.params.projectId;
+    const project = await ProjectsDAO.findAll(projectId);
+    res.json(project);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
- const findProject = async (req, res) => {
-    try {
-        const projectId = req.params.projectId;
-        const project = await ProjectsDAO.findAll(projectId);
-        res.json(project);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-} 
-
- const coursesFeed = async (req, res) => {
-    try {
-        const courseId = req.params.courseId;
-        const projects = await findOwnerId(courseId);
-        res.json(projects);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+const findCourseProject = async (req, res) => {
+  try {
+    const courseId = req.params.courseId;
+    const project = await findOwnerId(courseId);
+    res.json(project);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
- const findCourseProject = async (req, res) => {
-    try {
-        const courseId = req.params.courseId;
-        const project = await findOwnerId(courseId);
-        res.json(project);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
 
- const myProjects = async (req, res) => {
-    try {
-        const projectId = req.params.projectId;
-        const project = await findById(projectId);
-        res.json(project);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+const findProjectById = async (req, res) => {
+  try {
+    const projectId = req.params.projectId;
+    const project = await findById(projectId);
+    res.json(project);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
-
- const findProjectById = async (req, res) => {
-    try {
-        const projectId = req.params.projectId;
-        const project = await findById(projectId);
-        res.json(project);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
 
 const updateProject = async (req, res) => {
-    try {
-        const projectId = req.params.projectId;
-        const updatedProject = req.body;
-        const result = await update(projectId, updatedProject);
-        res.json(result);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  try {
+    const projectId = req.params.projectId;
+    const updatedProject = req.body;
+    const result = await update(projectId, updatedProject);
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
- const deleteProject = async (req, res) => {
-    try {
-        const projectId = req.params.projectId;
-        const result = await deleteById(projectId);
-        res.json(result);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+const deleteProject = async (req, res) => {
+  try {
+    const projectId = req.params.projectId;
+    const result = await deleteById(projectId);
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export default ProjectsController;
--- users/users-controller.js
+++ users/users-controller.js
@@ -1,20 +1,36 @@
 import * as userDao from "./users-dao.js";
 
 const UserController = (app) => {
-  app.post('/api/users', createUser);
-  app.get('/api/users', findAllUsers);
-  app.post('/api/users/multiple', getMultipleUsersByID);
+  app.post("/api/users", createUser);
+  app.get("/api/users", findAllUsers);
+  app.post("/api/users/multiple", getMultipleUsersByID);
+  app.delete("/api/users/:uid", deleteUser);
+  app.put("/api/users/:uid", updateUser);
 };
 
 const createUser = async (req, res) => {
   try {
     const newUser = await userDao.createUser(req.body);
-    res.status(201).json(newUser); 
+    res.status(201).json(newUser);
   } catch (error) {
     console.error("Error creating user:", error);
     res.status(500).json({ error: error.message });
   }
 };
+
+const updateUser = async (req, res) => {
+    const uid = req.params.uid;
+   
+    const updateUser = await userDao.updateUser(uid);
+    res.status(200).json(updateUser);
+};
+
+const deleteUser = async (req, res) => {
+    const uid = req.params.uid;
+    const status = await userDao.deleteUser(uid);
+    res.json(status);
+};
+  
 
 const findAllUsers = async (req, res) => {
   try {
@@ -30,15 +46,16 @@ const getMultipleUsersByID = async (req, res) => {
   try {
     const userIds = req.body.ids;
     if (!Array.isArray(userIds)) {
-      return res.status(400).json({ error: 'Expected an array of user IDs' });
+      return res.status(400).json({ error: "Expected an array of user IDs" });
     }
 
     const users = await userDao.findUsersByIds(userIds);
     res.status(200).json(users);
   } catch (error) {
-    console.error("Error fetching multiple users by IDs:", error); 
+    console.error("Error fetching multiple users by IDs:", error);
     res.status(500).json({ error: error.message });
   }
 };
+
 
 export default UserController;
